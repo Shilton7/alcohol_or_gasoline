6019490f7d9141d8249197fa4b17727655874171
Shilton <shilton.sistemas@gmail.com>
2020-06-13 16:10:38 -0300

    Including fonts and images

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:alcohol_or_gasoline/widgets/logo.widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Álcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: ListView(
+          children: <Widget>[Logo()],
+        ));
+  }
+}
